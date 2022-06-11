d3a1a666962df094965bdb1a3bb7260375e37900
joosecj <josecarloscjj@gmail.com>
2022-06-11 16:31:42 -0300

    finish home e page not found

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,15 @@
+import React, { Component }  from 'react';
+import { Link } from 'react-router-dom';
+import './erro.css';
+
+function Erro(){
+    return(
+        <div className='not-found'>
+            <h1>404</h1>
+            <h2>Página não econtrada</h2>
+            <Link to="/">Veja todos filmes!</Link>
+        </div>
+    )
+}
+
+export default Erro;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React, { Component }  from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
-
+import Erro from './pages/Erro';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
@@ -15,6 +15,8 @@ function RoutesApp(){
         <Routes>
             <Route path='/' element={ <Home/>} />
             <Route path='/filme/:id' element={ <Filme/> } /> 
+
+            <Route path="*" element={ <Erro/> } />
         </Routes>
     </BrowserRouter>
     )
