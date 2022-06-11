dc7bd125df3288f306eaf39a28bcff8100e3b8dd
joosecj <josecarloscjj@gmail.com>
2022-06-11 16:15:12 -0300

    Updating Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React, { Component }  from 'react';
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
 
 function Home(){
     const [filmes, setFilmes] = useState([]);
@@ -14,15 +17,28 @@ function Home(){
                     page: 1,
                 }
             })
-            console.log(response.data.results);
+           // console.log(response.data.results.slice(0,10));
+           setFilmes(response.data.results.slice(0,10))
         }
         loadFilmes();
 
     }, [])
 
     return(
-        <div>
-        <h1>BEM VINDO A HOME</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+
+            </div>
+        
         </div>
     )
 }
